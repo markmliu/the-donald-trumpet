b1c9b6020bd9cd3ea9c51fd6949f6081ff92b3f3
markmliu <uilkram@gmail.com>
2017-01-02 00:43:50 -0800

    ignore tweets by not donald, as well as retweets

--- the_donald.py
+++ the_donald.py
@@ -8,14 +8,22 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+DONALD_TRUMP_USER_ID = 25073877
+
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
-    def on_data(self, data):
-        tweet_text =  simplejson.loads(data)["text"].encode('ascii', 'ignore')
+    def on_status(self, status):
+        # Ignore tweets by anyone other than Trump.
+        if status.author.id != DONALD_TRUMP_USER_ID:
+            return True
+        tweet_text = status.text.encode('ascii', 'ignore')
+        # Ignore retweets.
+        if  'RT @' in tweet_text:
+            return True
+        print tweet_text        
         ps = subprocess.Popen(('echo', tweet_text), stdout=subprocess.PIPE)
         subprocess.call(('say'), stdin=ps.stdout)
-        #subprocess.system("echo '{}' | say ".format(tweet_text.encode('ascii', 'ignore')))
         return True
 
     def on_error(self, status):
@@ -27,5 +35,6 @@ if __name__ == '__main__':
     auth = OAuthHandler(api_keys.consumer_key, api_keys.consumer_secret)
     auth.set_access_token(api_keys.access_token, api_keys.access_token_secret)
     stream = Stream(auth, l)
+    
     # Filter to only Donald Trump's tweets.
-    stream.filter(follow=['25073877'])
+    stream.filter(follow=[str(DONALD_TRUMP_USER_ID)])
